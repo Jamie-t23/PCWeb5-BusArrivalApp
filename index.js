0e6ad77cd451646b0c6a553e725a0a6ed8a4052e
Tay Jia Hui <tayjiahui95@hotmail.com>
2023-08-30 20:00:24 +0800

    Initial Commit
    Initial Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+const { default: axios } = require("axios")
+
+const busStop = "18141"
+const api = `http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=${busStop}`
+
+
+const config = {
+  headers: {
+    'AccountKey': 'xHrBZD3HTbqeGcgRVnd/1A=='
+  }
+}
+
+async function loadBusData() {
+  console.log("start")
+  const res = await axios.get(api,config)
+   // .then((res) => console.log(res.status))
+  //.catch((err) => {
+  //  console.log(err)
+  //})
+  // console.log("end")
+
+  console.log(res.data.Services)
+  console.log("end")
+}
+
+loadBusData()
